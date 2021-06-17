d0fbb68e333c5511c15194a0c83522e31806c0b8
igorWebMASTER <igor_1917@hotmail.com>
2021-06-17 00:57:05 -0300

    style: change styles of profile page

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -4,10 +4,32 @@ import { shade } from 'polished';
 import SignInBackgroundImg from '../../assets/sign-up-background.png';
 
 export const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  align-items: stretch;
+  > header {
+    height: 144px;
+    position: relative;
+    background: #28262e;
+
+    display: flex;
+    align-content: center;
+
+    div {
+      width: 100%;
+      margin: 0 auto;
+      padding: 0 2rem;
+      max-width: 1120px;
+
+      svg {
+        position: absolute;
+        top: 50px;
+        color: #999591;
+        width: 24px;
+        height: 24px;
+      }
+    }
+  }
+
+  /* display: flex;
+  align-items: stretch; */
 `;
 
 export const Content = styled.div`
@@ -15,7 +37,7 @@ export const Content = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin: 0 auto;
+  margin: -176px auto 0;
 
   width: 100%;
   max-width: 700px;
@@ -24,6 +46,8 @@ export const Content = styled.div`
     margin: 80px 0;
     width: 340px;
     text-align: center;
+    display: flex;
+    flex-direction: column;
 
     h1 {
       margin-bottom: 24px;
@@ -42,6 +66,10 @@ export const Content = styled.div`
         color: ${shade(0.2, '#f4ede8')};
       }
     }
+
+    input[name='old_password'] {
+      margin-top: 24px;
+    }
   }
 `;
 
@@ -49,4 +77,49 @@ export const Background = styled.div`
   flex: 1;
   background: url(${SignInBackgroundImg}) no-repeat center;
   background-size: cover;
+`;
+
+export const AvatarInput = styled.div`
+  margin-bottom: 32px;
+  position: relative;
+  width: 186px;
+
+  align-self: center;
+
+  img {
+    max-width: 186px;
+    height: 186px;
+    border-radius: 50%;
+  }
+
+  label {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    border-radius: 50%;
+    background: #ff9000;
+    height: 48px;
+    width: 48px;
+    cursor: pointer;
+
+    border: 0;
+    transition: background-color 0.2s;
+
+    display: grid;
+    place-content: center;
+
+    input {
+      display: none;
+    }
+
+    svg {
+      width: 20px;
+      height: 20px;
+      color: #312e38;
+    }
+
+    &:hover {
+      background: ${shade(0.2, '#ff9000')};
+    }
+  }
 `;
